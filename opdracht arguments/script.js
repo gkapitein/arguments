c9efd08bfc5bf4c5ee5b9e775614f2e5e4c2fe75
gkapitein <84490555+gkapitein@users.noreply.github.com>
2021-06-10 20:02:28 +0200

    Add files via upload

---
+++ opdracht arguments/script.js
@@ -0,0 +1,10 @@
+const paintWall = function (color, colora) {
+    console.log("the wall has been painted" + color);
+    console.log("The north wall has been painted" + colora);
+}
+
+const color = "red"
+const colora = "green"
+
+paintWall(color, colora);
+
